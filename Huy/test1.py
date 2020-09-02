511c08a564334bde7b72566eda4875ebaaf13736
Tran-Minh-Quan <minhquan310599@gmail.com>
2020-09-03 03:10:58 +0700

    Chỉnh hàm tính edges, edge_num, chỉnh hệ số

--- Huy/test1.py
+++ Huy/test1.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-import func as f
+import time
+import Huy.func as f
 import matplotlib.pyplot as plt
 plt.ion()
 def testthreshold(winName='tên của window',cam=1):
@@ -14,13 +15,15 @@ def testthreshold(winName='tên của window',cam=1):
   def Trackchanged(x):
     pass
   cv2.namedWindow(winName)
-  cv2.createTrackbar("THKersize", winName, 11, 897, Trackchanged)
-  cv2.createTrackbar("C", winName, 48,255 , Trackchanged)
+  cv2.createTrackbar("THKersize", winName, 241, 897, Trackchanged)
+  cv2.createTrackbar("C", winName, 15,255 , Trackchanged)
   cv2.createTrackbar('OpKerSize',winName,3,20,Trackchanged)
   cv2.createTrackbar('ErsKerSize', winName, 5, 20, Trackchanged)
   if isinstance(cam, str):
     cv2.createTrackbar('frame', winName, 8, frameNum, Trackchanged)
+  pre_time = time.time()
   while (True):
+
     THKersize = cv2.getTrackbarPos("THKersize", winName)
     C = cv2.getTrackbarPos("C", winName)
     OpKerSize = cv2.getTrackbarPos("OpKerSize", winName)
@@ -37,18 +40,18 @@ def testthreshold(winName='tên của window',cam=1):
       if ret == True:
         frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         frame1 = frame[75:95, 220:420]
-        template = cv2.imread('doanday2.jpg', 0)
+        template = cv2.imread('template.jpg', 0)
         w, h = template.shape[::-1]
         res = cv2.matchTemplate(frame1, template, cv2.TM_CCOEFF)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
-        bottom_right = (top_left[0] + w - 10, top_left[1] + h)
+        bottom_right = (top_left[0] + w, top_left[1] + h)
 
         frame2 = frame1[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]].copy()
         #cv2.rectangle(frame1, top_left, bottom_right, (0.0, 255), 2)
 
         opening_kernel = np.ones((OpKerSize, OpKerSize), np.uint8)
-
+        dilation_kernel = np.ones((3, 3), np.uint8)
         #if blsize % 2 == 0:
           #pass
         #else:
@@ -59,7 +62,7 @@ def testthreshold(winName='tên của window',cam=1):
         #th1 = cv2.adaptiveThreshold(frame1,255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, THKersize, C)
         #opening = f.main('dilation',opening_kernel,f.main('erosion',opening_kernel,th))
         opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, opening_kernel)
-        opening1= cv2.morphologyEx(opening,cv2.MORPH_CLOSE,opening_kernel )
+        #opening1= cv2.morphologyEx(opening,cv2.MORPH_CLOSE,opening_kernel )
 
         #erosion_kernel = np.zeros((ErsKerSize, ErsKerSize), np.uint8)
         #erosion_kernel[int((ErsKerSize-1)/2),:]=1
@@ -68,12 +71,16 @@ def testthreshold(winName='tên của window',cam=1):
 
         #edges = cv2.Canny(opening, 50, 200, apertureSize=3)
         #edges = opening - erosion;
-        edges = cv2.Canny(opening1, 50, 200, apertureSize=3)
-        num1, count1 = np.unique(edges, return_counts=True)
+
+        dilation = cv2.dilate(opening,dilation_kernel)
+        edges = dilation - opening
+        #edges = cv2.Canny(opening, 50, 200, apertureSize=3)
+        '''num1, count1 = np.unique(edges, return_counts=True)
         if num1[-1]==255:
           edge_num=count1[-1]
         else:
-          edge_num=0
+          edge_num=0'''
+        edge_num = np.sum(edges)//255
         print(edge_num)
         y[0,frameIndex]=edge_num
         plt.clf()
@@ -101,16 +108,16 @@ def testthreshold(winName='tên của window',cam=1):
         edge_title[:] = [220]
         cv2.putText(img=edge_title, text='Edge image', fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=0,thickness=1,
                     lineType=cv2.LINE_AA, org=(55,75))
-        if edge_num > 45:
-          cv2.rectangle(frame, (220, 75), (420, 95), (255, 255, 255), 2)
+        if edge_num > 47:
+          cv2.rectangle(frame, (220+top_left[0], 75+top_left[1]), (220+bottom_right[0], 75+bottom_right[1]), (255, 255, 255), 2)
           cv2.putText(img=frame, text='ERROR', fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=255,thickness=2, org=(150, 90))
         else:
-          cv2.rectangle(frame, (220, 75), (420, 95), (0, 0, 0), 2)
+          cv2.rectangle(frame, (220+top_left[0], 75+top_left[1]), (220+bottom_right[0], 75+bottom_right[1]), (0, 0, 0), 2)
           cv2.putText(img=frame, text='NORMAL', fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=0, thickness=2, org=(150, 90))
         #all = cv2.vconcat([th_title, th, op_title, opening, edge_title, edges])
         all = cv2.hconcat([frame, cv2.vconcat([th_title, th, op_title, opening, edge_title, edges])])
 
-        cv2.imshow('calib',all)
+        cv2.imshow(winName,all)
         #edges = cv2.Canny(opening, 50, 200, apertureSize=3)
         #feat = np.sum(edges / 255)
         #print(feat)
