42315608df95c1c17f0aaca4c6eccfa5e9531c31
thotranhuuu99 <thotranhuu99@gmail.com>
2020-09-12 13:32:36 +0700

    Add normal object example

--- Tho/Main_lib/Example_usage.py
+++ Tho/Main_lib/Example_usage.py
@@ -1,10 +1,9 @@
 import cv2
 from distance_calc import CircleDistance
 import time
-import numpy as np
 
 
-# Canny_low = 200, Canny_high = 600, step_size = 5, hough_param =22,
+# Canny_low = 200, Canny_high = 5000, step_size = 1, hough_param =22,
 # slope = 867.7887424687945, intercept = -0.18242145320198588
 Circle_based_estimate_1 = CircleDistance(50, 5000, 1, 22, 867.7887424687945, -0.18242145320198588)
 time_start = time.time()
@@ -16,10 +15,12 @@ img = cv2.imread("./Changed_data/26.jpg")
 distance_2, img_out_2, error_2 = Circle_based_estimate_1.calculate(img, (100, 200), (300, 400),
                                                              0.2, mode=1, object_width=10)
 time_stop_2 = time.time()
-img = cv2.imread("./Changed_data/28.jpg")
-distance_3, img_out_3, error_3 = Circle_based_estimate_1.calculate(img, (200, 300), (400, 500),
-                                                             0.2, mode=1, object_width=10)
+img = cv2.imread("./Changed_data/test_2.jpg")
+Circle_based_estimate_1.first_detect = 1
+distance_3, img_out_3, error_3 = Circle_based_estimate_1.calculate(img, (50, 300), (210, 550),
+                                                             0.1, mode=1, object_width=10)
 time_stop_3 = time.time()
+cv2.imwrite("img_test.jpg", img_out_3)
 
 cv2.imshow("Window 1", img_out_1)
 cv2.waitKey(1000)
