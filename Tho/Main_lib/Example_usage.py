32ef98e3d58540fb5792c13a60a67748aacb6615
thotranhuuu99 <thotranhuu99@gmail.com>
2020-09-12 08:39:41 +0700

    Add Param estimate based on last detect

--- Tho/Main_lib/Example_usage.py
+++ Tho/Main_lib/Example_usage.py
@@ -3,16 +3,31 @@ from distance_calc import CircleDistance
 import time
 import numpy as np
 
-img = cv2.imread("./Changed_data/25.jpg")
+
 # Canny_low = 200, Canny_high = 600, step_size = 5, hough_param =22,
 # slope = 867.7887424687945, intercept = -0.18242145320198588
-Circle_based_estimate_1 = CircleDistance(200, 600, 1, 22, 867.7887424687945, -0.18242145320198588)
+Circle_based_estimate_1 = CircleDistance(50, 5000, 1, 22, 867.7887424687945, -0.18242145320198588)
 time_start = time.time()
-distance, img_out, error = Circle_based_estimate_1.calculate(img, (100, 200), (300, 400),
+img = cv2.imread("./Changed_data/25.jpg")
+distance_1, img_out_1, error_1 = Circle_based_estimate_1.calculate(img, (100, 200), (300, 400),
                                                              0.2, mode=1, object_width=10)
-time_stop = time.time()
-print("Distance: %.2f" % distance)
-print("Error code: %.2f" % error)
-print(time_stop-time_start)
-cv2.imshow("Window 1", img_out)
+time_stop_1 = time.time()
+img = cv2.imread("./Changed_data/26.jpg")
+distance_2, img_out_2, error_2 = Circle_based_estimate_1.calculate(img, (100, 200), (300, 400),
+                                                             0.2, mode=1, object_width=10)
+time_stop_2 = time.time()
+img = cv2.imread("./Changed_data/28.jpg")
+distance_3, img_out_3, error_3 = Circle_based_estimate_1.calculate(img, (100, 200), (300, 400),
+                                                             0.2, mode=1, object_width=10)
+time_stop_3 = time.time()
+
+cv2.imshow("Window 1", img_out_1)
 cv2.waitKey(1000)
+cv2.imshow("Window 1", img_out_2)
+cv2.waitKey(1000)
+cv2.imshow("Window 1", img_out_3)
+cv2.waitKey(1000)
+print("Distance: %.2f\nError code: %d\nTime run: %.4f" % (distance_1, error_1, time_stop_1-time_start))
+print("Distance: %.2f\nError code: %d\nTime run: %.4f" % (distance_2, error_2, time_stop_2-time_stop_1))
+print("Distance: %.2f\nError code: %d\nTime run: %.4f" % (distance_3, error_3, time_stop_3-time_stop_2))
+
