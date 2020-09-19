3bf389ab2a4f10453779179f4acf018200bd35c1
nguyenphan1999 <67010443+nguyenphan1999@users.noreply.github.com>
2020-09-20 00:19:19 +0700

    Add files via upload

---
+++ Nguyen/segmentation.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageDraw, ImageFilter
+import cv2
+from cv2 import resize
+
+# im1 = Image.open("098.png")
+# im2 = Image.open("blue_sky-1152x864.jpg")
+#
+# im2.paste(im1, (0, 0))
+# im2.show()
+#
+# mask=Image.open("seg.jpg")
+#   # im = Image.composite(im1, im2, mask)
+# im=im2.copy()
+# im.paste(im1,(0,0),mask)
+# im.save("t.jpg")
+# im.show()
+def Reverse(tuples):
+    new_tup = tuples[::-1]
+    return new_tup
+
+background= cv2.imread('blue_sky-1152x864.jpg')
+foreground = cv2.imread('098.png')
+foreground=cv2.resize(foreground,Reverse(background.shape[:-1]))
+
+
+gray=cv2.imread('098.png',0)
+_,mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
+mask=cv2.resize(mask,Reverse(background.shape[:-1]))
+mask_inv = cv2.bitwise_not(mask)
+
+cv2.imwrite('b.jpg',mask_inv)
+fg=cv2.bitwise_and(foreground,foreground,None,mask)
+bg=cv2.bitwise_and(background,background,None,mask_inv)
+dst = cv2.add(bg,fg)
+
+
+cv2.imwrite("t.jpg",dst)
