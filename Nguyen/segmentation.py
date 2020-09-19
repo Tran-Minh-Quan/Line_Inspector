9e2bca368a4e50c1323ea2c763219b3353cd4453
nguyenphan1999 <67010443+nguyenphan1999@users.noreply.github.com>
2020-09-20 01:01:55 +0700

    thêm chú thích

--- Nguyen/segmentation.py
+++ Nguyen/segmentation.py
@@ -18,12 +18,12 @@ def Reverse(tuples):
     new_tup = tuples[::-1]
     return new_tup
 
-background= cv2.imread('blue_sky-1152x864.jpg')
-foreground = cv2.imread('098.png')
+background= cv2.imread('blue_sky-1152x864.jpg')  #thay = tên của file muốn làm background
+foreground = cv2.imread('098.png') #thay = tên của file muốn làm foreground
 foreground=cv2.resize(foreground,Reverse(background.shape[:-1]))
 
 
-gray=cv2.imread('098.png',0)
+gray=cv2.imread('098.png',0) #thay = tên của file muốn làm foreground
 _,mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
 mask=cv2.resize(mask,Reverse(background.shape[:-1]))
 mask_inv = cv2.bitwise_not(mask)
@@ -34,4 +34,4 @@ bg=cv2.bitwise_and(background,background,None,mask_inv)
 dst = cv2.add(bg,fg)
 
 
-cv2.imwrite("t.jpg",dst)
+cv2.imwrite("t.jpg",dst) #thay = tên của file kết quả ra
